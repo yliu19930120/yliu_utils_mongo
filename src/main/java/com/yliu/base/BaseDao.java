7e0cfa39bd9954066ebcb8cdc7a69403186c0bbe
LiuYonghua <yliu19930120@163.com>
2019-08-03 09:45:04 +0800

    加入大成jar的配置

--- src/main/java/com/yliu/base/BaseDao.java
+++ src/main/java/com/yliu/base/BaseDao.java
@@ -1,7 +1,11 @@
 package com.yliu.base;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.bson.Document;
 import org.slf4j.Logger;
+
 import com.mongodb.client.MongoCollection;
 import com.yliu.utils.MongoUtil;
 
@@ -19,5 +23,19 @@ public abstract class BaseDao<T> {
 		Document doc = MongoUtil.adaptToDocument(t);
 		collection.insertOne(doc);
 		return 1;
+	}
+	
+	public int save(List<T> list){
+		MongoCollection<Document> collection = 
+				MongoUtil.getCollection(getDbName(), getCollName());
+		List<Document> docs = list.stream().map(t->MongoUtil.adaptToDocument(t)).collect(Collectors.toList());
+		collection.insertMany(docs);
+		return list.size();
+	}
+	
+	public MongoCollection<Document> getColection(){
+		MongoCollection<Document> collection = 
+				MongoUtil.getCollection(getDbName(), getCollName());
+		return collection;
 	}
 }
